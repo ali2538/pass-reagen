410bd14a9e243b4e4ce6cc1a7a9d8e00219f05e2
Ali Hadizadeh <ali@hadizadeh.com>
2024-04-12 00:33:24 -0400

    some changes

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: "/ali2538",
-
+  assetPrefix: "/ali2538",
   /**
    * Disable server-based image optimization. Next.js does not support
    * dynamic features with static exports.
